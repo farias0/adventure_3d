5c03d86717812baca319a8c0273d3bb75db208ff
Junior Farias <fariasjunior96@gmail.com>
2024-08-02 00:37:40 -0300

    Simplified enemy logic

--- Assets/Scripts/Enemy1Movement.cs
+++ Assets/Scripts/Enemy1Movement.cs
@@ -13,14 +13,12 @@ public class Enemy1Movement : MonoBehaviour
     public float SpeedWalking;
     public float SpeedRunning;
     public float RotationSpeed;
-    public float DistanceChaseWalk;
     public float DistanceChaseRun;
+    public float DistanceChaseWalk;
     public float DistanceChaseStop;
 
     private Animator mAnimator;
     private Rigidbody mRigidbody;
-    private EnemyMode mMode = EnemyMode.Idle;
-    private float mSpeed;
 
     // Start is called before the first frame update
     void Start()
@@ -32,43 +30,49 @@ public class Enemy1Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         AnimationMoveState animationMoveState;
         float distanceFromPlayer = Vector3.Distance(transform.position, Player.transform.position);
 
-        if (distanceFromPlayer < DistanceChaseStop || distanceFromPlayer > DistanceChaseWalk)
+
+        if (distanceFromPlayer < DistanceChaseStop)
         {
-            mMode = EnemyMode.Idle;
-            mSpeed = 0;
+            RotateTowardsPlayer();
             animationMoveState = AnimationMoveState.Idle;
+        }
+        else if (distanceFromPlayer < DistanceChaseWalk)
+        {
+            RotateTowardsPlayer();
+            MoveTowardsPlayer(SpeedWalking);
+            animationMoveState = AnimationMoveState.Walk;
         }
         else if (distanceFromPlayer < DistanceChaseRun)
         {
-            mMode = EnemyMode.Chasing;
-            mSpeed = SpeedWalking;
-            animationMoveState = AnimationMoveState.Walk;
+            RotateTowardsPlayer();
+            MoveTowardsPlayer(SpeedRunning);
+            animationMoveState = AnimationMoveState.Run;
         }
         else
         {
-            mMode = EnemyMode.Chasing;
-            mSpeed = SpeedRunning;
-            animationMoveState = AnimationMoveState.Run;
-        }
-
-        if (mMode != EnemyMode.Idle)
-        {
-            // Rotate towards player
-            Vector3 rotateDirection = new(Player.transform.position.x, 0, Player.transform.position.z);
-            Quaternion targetRotation = Quaternion.LookRotation(rotateDirection - transform.position);
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, RotationSpeed * Time.deltaTime);
-
-
-            // Move towards player
-            Vector3 moveDirection = (Player.transform.position - transform.position).normalized;
-            mRigidbody.MovePosition(transform.position + mSpeed * Time.deltaTime * moveDirection);
+            animationMoveState = AnimationMoveState.Idle;
         }
 
 
         mAnimator.SetInteger("MovementState", animationMoveState.GetHashCode());
+
+
+        //if (Input.GetKeyDown(KeyCode.N)) mAnimator.SetTrigger("GetHit");
+    }
+
+    void MoveTowardsPlayer(float speed)
+    {
+        Vector3 moveDirection = (Player.transform.position - transform.position).normalized;
+        mRigidbody.MovePosition(transform.position + speed * Time.deltaTime * moveDirection);
+    }
+
+    void RotateTowardsPlayer()
+    {
+        Vector3 rotateDirection = new(Player.transform.position.x, 0, Player.transform.position.z);
+        Quaternion targetRotation = Quaternion.LookRotation(rotateDirection - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, RotationSpeed * Time.deltaTime);
     }
 }
